ea685269b81541673370a7a4e9f7fe7ddde73471
Hiroyuki Muto <mutopsy@omu.ac.jp>
2025-02-27 23:54:22 +0900

    update

--- R/hdi.r
+++ R/hdi.r
@@ -3,73 +3,56 @@
 #' This function calculates the highest density interval (HDI) for a given
 #' set of samples. The HDI is the interval that contains a specified
 #' proportion (default 95%) of the highest density values from the distribution.
+#'
+#' @param x A numeric vector of data values for which the HDI will be calculated.
+#' @param prob The probability mass of the HDI to calculate. Default is 0.95.
+#'
+#' @return A numeric vector of length 2 representing the lower and upper bounds of the HDI.
+#'
+#' @details The function first sorts the input data and computes the necessary quantiles using inear interpolation.
+#' It then identifies the interval that contains the highest density, ensuring that the HDI encompasses the specified probability mass.
 #' Note that this function does not support the case where two or more separate
 #' HDIs exist.
 #'
-#' @param x A numeric vector of samples from the distribution.
-#' @param prob A numeric value between 0 and 1 specifying the probability mass
-#'   for the HDI (default is 0.95).
-#'
-#' @return A numeric vector containing the values within the HDI.
-#'
 #' @examples
-#' # Generate some random posterior samples
 #' set.seed(123)
-#' samples <- rnorm(1000, mean = 0, sd = 1)
-#'
-#' # Compute the 95% HDI
-#' hdi(samples, prob = 0.95)
+#' data <- rnorm(1000)  # Generate random normal data
+#' hdi2(data, prob = 0.95)
 #'
 #' @export
 
 hdi <- function(x, prob = 0.95) {
-
-  # vector size
+  # Vector size
   n <- length(x)
 
-  # sort
+  # Sort the data
   sorted_x <- sort(x)
 
-  # interval length
-  interval_length <- round(prob * n)
+  # Interval length (using n-1 to match quantile() behavior)
+  interval_length <- (prob * (n - 1)) + 1
 
-  hdi_est <- NULL
+  # Calculate indices
+  lower_idx <- (1:(n - interval_length + 1))
+  upper_idx <- lower_idx + interval_length - 1
+
+  # Fractional part of the indices
+  lower_frac <- lower_idx - floor(lower_idx)
+  upper_frac <- upper_idx - floor(upper_idx)
+
+  # Calculate interpolated boundary values
+  lower_val <- (1 - lower_frac) * sorted_x[floor(lower_idx)] + lower_frac * sorted_x[ceiling(lower_idx)]
+  upper_val <- (1 - upper_frac) * sorted_x[floor(upper_idx)] + upper_frac * sorted_x[ceiling(upper_idx)]
+
+  # Search for the minimum width of the HDI
   min_width <- Inf
+  hdi_est <- c(NA, NA)
 
-  for (i in 1:(n - interval_length + 1)) {
-    current_interval <- sorted_x[i:(i + interval_length - 1)]
-    current_width <- diff(range(current_interval))
+  for (i in seq_along(lower_val)) {
+    current_width <- upper_val[i] - lower_val[i]
 
     if (current_width < min_width) {
       min_width <- current_width
-      hdi_est <- range(current_interval)
-    }
-  }
-
-  return(hdi_est)
-}
-
-hdi <- function(x, prob = 0.95) {
-
-  # vector size
-  n <- length(x)
-
-  # sort
-  sorted_x <- sort(x)
-
-  # interval length
-  interval_length <- ceiling(prob * n)
-
-  hdi_est <- NULL
-  min_width <- Inf
-
-  for (i in 1:(n - interval_length + 1)) {
-    current_interval <- sorted_x[i:(i + interval_length - 1)]
-    current_width <- diff(range(current_interval))
-
-    if (current_width < min_width) {
-      min_width <- current_width
-      hdi_est <- range(current_interval)
+      hdi_est <- c(lower_val[i], upper_val[i])
     }
   }
 
